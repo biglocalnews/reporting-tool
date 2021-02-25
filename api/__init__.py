99acc9539f4676adec9113aca410e6a5c0817f2c
nmanduj1 <nmandujano1@gmail.com>
2021-02-25 16:54:10 -0500

    Add dunder file handling flask app initialization

---
+++ api/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# leaving in as placeholder
+# app.config["SQLALCHEMY_DATABASE_URI"] = ""
+# db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'Hello!'
+
+    
+
