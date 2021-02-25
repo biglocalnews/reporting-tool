5f203a5f680535b39c703025cbb669473869f3e4
nmanduj1 <nmandujano1@gmail.com>
2021-02-25 17:03:22 -0500

    Set up resolver bindings for Query and Mutation

--- api/app.py
+++ api/app.py
@@ -1,12 +1,36 @@
-from flask import Flask
-from flask_cors import CORS
+from api import app
 
-app = Flask(__name__)
-CORS(app)
+from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, ObjectType
+from ariadne.constants import PLAYGROUND_HTML
+from flask import request, jsonify
+from api.queries import resolve_users
+from api.mutations import resolve_create_user
 
-@app.route('/')
-def say_hello_world():
-    return "Hello, world!"
+query = ObjectType("Query")
+mutation = ObjectType("Mutation")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+query.set_field("users", resolve_users)
+mutation.set_field("createUser", resolve_create_user) # feels like a camelCase vs under_score headache is looooming-
+
+type_defs = load_schema_from_path("schema.graphql")
+schema = make_executable_schema(
+    type_defs, query, mutation, snake_case_fallback_resolvers
+)
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
