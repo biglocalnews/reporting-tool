1b330940ff51ddcf3393b75f2964389295ffdd48
Ro <ro@Ros-MacBook-Pro.fios-router.home>
2021-03-01 17:07:03 -0500

    Add heads bottom border image to header nav

--- client/src/components/AppHeader.tsx
+++ client/src/components/AppHeader.tsx
@@ -9,41 +9,44 @@ const { Header } = Layout;
 export default class AppHeader extends React.Component {
   render() {
     return (
-      <Header className="header">
-        <Row wrap={false}>
-          <Col flex="none">
-            <div className="header__logo">
-              <img src={logo} alt="App logo" />
-            </div>
-          </Col>
-          <Col flex="auto">
-            <Menu
-              theme="light"
-              mode="horizontal"
-              style={{ float: "right", lineHeight: "64px" }}
-            >
-              <Menu.Item
-                disabled
-                key="userProfileImage"
-                className="header__user-profile-item"
+      <div>
+        <Header className="header">
+          <Row wrap={false}>
+            <Col flex="none">
+              <div className="header__logo">
+                <img src={logo} alt="App logo" />
+              </div>
+            </Col>
+            <Col flex="auto">
+              <Menu
+                theme="light"
+                mode="horizontal"
+                style={{ float: "right", lineHeight: "64px" }}
               >
-                <Avatar
-                  size="large"
-                  style={{ backgroundColor: "#87d068" }}
-                  icon={<UserOutlined />}
-                />
-              </Menu.Item>
-              <Menu.Item
-                disabled
-                key="userProfileName"
-                className="header__user-profile-item"
-              >
-                <h5>Tina Turner</h5>
-              </Menu.Item>
-            </Menu>
-          </Col>
-        </Row>
-      </Header>
+                <Menu.Item
+                  disabled
+                  key="userProfileImage"
+                  className="header__user-profile-item"
+                >
+                  <Avatar
+                    size="large"
+                    style={{ backgroundColor: "#87d068" }}
+                    icon={<UserOutlined />}
+                  />
+                </Menu.Item>
+                <Menu.Item
+                  disabled
+                  key="userProfileName"
+                  className="header__user-profile-item"
+                >
+                  <h5>Tina Turner</h5>
+                </Menu.Item>
+              </Menu>
+            </Col>
+          </Row>
+        </Header>
+        <div className="header__border-image"></div>
+      </div>
     );
   }
 }
