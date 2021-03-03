f1f6ded80ae1ccd78fc5e1ffd63d6ce4ff7cdc9e
Ro E <seerocode@users.noreply.github.com>
2021-03-03 16:39:07 -0500

    BBC UI components setup (#8)
    * Remove stock logo
    
    * Update manifest
    
    * Add logo and border image
    
    * Add components for UI layout
    - Added sidebar
    - Added header for avatar and user name
    - Added content pane and footer beneath it
    - Added a sample table for the dataset page
    
    * Update default app to include new components
    
    * Add heads bottom border image to header nav
    
    * Move out teams table code
    
    * Fix avatar and user name placement
    
    * Change app name
    
    Co-authored-by: Ro <ro@Ros-MacBook-Pro.fios-router.home>

---
+++ client/src/components/AppHeader.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./AppHeader.css";
+import logo from "../assets/5050logo.jpg";
+import { Layout, Avatar, Col, Row } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+const { Header } = Layout;
+
+export default class AppHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header className="header">
+          <Row wrap={false}>
+            <Col flex="none">
+              <div className="header__logo">
+                <img src={logo} alt="App logo" />
+              </div>
+            </Col>
+            <Col flex="auto">
+              <div
+                id="header__user"
+                style={{ float: "right", lineHeight: "64px" }}
+              >
+                <span id="avatar" className="header__user-profile-item">
+                  <Avatar
+                    style={{ backgroundColor: "#87d068" }}
+                    icon={<UserOutlined />}
+                  />
+                </span>
+                <span id="name" className="header__user-profile-item">
+                  <p>Tina Turner</p>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Header>
+        <div className="header__border-image"></div>
+      </div>
+    );
+  }
+}
