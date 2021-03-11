0a4e9cb472e5a439bcc1a3d2a838330f52a33199
Ro <ro@Ros-MacBook-Pro.fios-router.home>
2021-03-11 17:56:34 -0500

    Update AppSidebar test

--- client/src/__tests__/AppSidebar.test.tsx
+++ client/src/__tests__/AppSidebar.test.tsx
@@ -2,26 +2,39 @@ import { AppSidebar } from "../components/AppSidebar";
 import { shallow } from "enzyme"
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import uk from "../i18n/locales/en-gb/translation.json"
 import SubMenu from "antd/lib/menu/SubMenu";
 
-// Mock react-i18next
-i18next
-  .use(initReactI18next)
-  .init({
-    lng: "en-gb",
-    fallbackLng: "en-gb",
-    interpolation: {
-      escapeValue: false,
-    },
-    debug: true,
-    keySeparator: false,
-    resources: { "en-gb": { translation: uk } },
-    lowerCaseLng: true,
-  });
+describe('UK English internationalization', () => {
+  beforeAll(() => {
 
-it('Translates sidebar title text', () => {
-  console.log(uk)
-  const sidebar = shallow(<AppSidebar />);
-  expect(sidebar.find(SubMenu).first().prop('title')).toBe('My Programmes');
-})
+    // Mock react-i18next
+    i18next
+      .use(initReactI18next)
+      .init({
+        lng: "en-gb",
+        fallbackLng: "en-gb",
+        interpolation: {
+          escapeValue: false,
+        },
+        debug: true,
+        keySeparator: false,
+        resources: {
+          "en-gb": {
+            translation: {
+              "teamsSideBarTitle": "My Programmes"
+            }
+          },
+          "en": {
+            "teamsSideBarTitle": "My Programs"
+          }
+        },
+        lowerCaseLng: true,
+      });
+  })
+
+  test('translates sidebar title text to UK English', () => {
+    const sidebar = shallow(<AppSidebar />);
+    expect(sidebar.find(SubMenu).first().prop('title')).toBe('My Programmes');
+  });
+});
+
