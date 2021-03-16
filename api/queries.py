efe9259dd25ac1fffd0be8120a63dfd60e175b90
nmanduj1 <nmandujano1@gmail.com>
2021-03-16 16:59:28 -0400

    Add documentation for user query

--- api/queries.py
+++ api/queries.py
@@ -5,6 +5,12 @@ query = ObjectType("Query")
 @query.field("user")
 @convert_kwargs_to_snake_case
 def resolve_user(obj, info, user_id):
+    '''GraphQL query to find a user based on user ID.
+        :param obj: Describe what obj is
+        :param info: Describe what info is
+        :param user_id: The user's ID
+        :returns: Whatever payload is
+    '''
     payload = {
         'id': 1,
         'first_name': 'Cat',
