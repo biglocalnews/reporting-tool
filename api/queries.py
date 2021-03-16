2797e491218396c3a5e6530342e382dc360ee2b2
nmanduj1 <nmandujano1@gmail.com>
2021-03-16 16:54:22 -0400

    Remove specific team and program

--- api/queries.py
+++ api/queries.py
@@ -12,35 +12,3 @@ def resolve_user(obj, info, user_id):
     }
 
     return payload
-
-@query.field("teamUsers")
-@convert_kwargs_to_snake_case
-def resolve_teamUsers(obj, info, team_id):
-    payload = [{
-        'id': 3,
-        'first_name': 'Little',
-        'last_name': 'Berry Team!!'
-    },
-    {
-        'id': 4,
-        'first_name': 'Cherry',
-        'last_name': 'Berry!'
-    }]
-    
-    return payload
-
-@query.field("programUsers")
-@convert_kwargs_to_snake_case
-def resolve_programUsers(obj, info, program_id):
-    payload = [{
-        'id': 5,
-        'first_name': 'Cheery',
-        'last_name': 'Berry- Program!'
-    },
-    {
-        'id': 6,
-        'first_name': 'Blue',
-        'last_name': 'Berry!'
-    }]
-
-    return payload
