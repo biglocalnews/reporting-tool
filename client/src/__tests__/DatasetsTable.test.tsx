d1c75f1d15d3ef79120d90e429f059ed9e1ec474
Ro <roen@stanford.edu>
2021-03-29 16:34:25 -0400

    chore(ch112): Fix linting issues

--- client/src/__tests__/DatasetsTable.test.tsx
+++ client/src/__tests__/DatasetsTable.test.tsx
@@ -4,22 +4,22 @@ import { DatasetsTable } from "../components/DatasetsTable";
 import { axe } from "jest-axe";
 
 describe("accessibility", () => {
-    it("should not have basic accessibility issues", async () => {
-        Object.defineProperty(window, "matchMedia", {
-            writable: true,
-            value: jest.fn().mockImplementation((query) => ({
-                matches: false,
-                media: query,
-                onchange: null,
-                addListener: jest.fn(), // deprecated
-                removeListener: jest.fn(), // deprecated
-                addEventListener: jest.fn(),
-                removeEventListener: jest.fn(),
-                dispatchEvent: jest.fn(),
-            })),
-        });
-        const { container } = render(<DatasetsTable data={[]} columns={[]} />);
-        const results = await axe(container);
-        expect(results).toHaveNoViolations();
+  it("should not have basic accessibility issues", async () => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
     });
+    const { container } = render(<DatasetsTable data={[]} columns={[]} />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
