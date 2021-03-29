cf5cf76606827f64fc475ec2f844c15d4a8f6394
Ro <roen@stanford.edu>
2021-03-29 16:09:37 -0400

    feat(ch112): add user homepage component

---
+++ client/src/components/Home.tsx
@@ -0,0 +1,86 @@
+import React, { Suspense } from "react";
+import { Layout, Tag, Button } from "antd";
+import { DatasetsTable } from "./DatasetsTable";
+import { SearchAutoComplete } from "./SearchAutoComplete";
+import { PlusOutlined } from "@ant-design/icons";
+
+const { Footer, Content } = Layout;
+
+const columns: any = [
+  {
+    title: "Team",
+    dataIndex: "team",
+    key: "team",
+  },
+  {
+    title: "Dataset",
+    dataIndex: "dataset",
+    key: "dataset",
+  },
+  {
+    title: "Last Updated",
+    dataIndex: "lastUpdated",
+    key: "lastUpdated",
+  },
+  {
+    title: "Tags",
+    key: "tags",
+    dataIndex: "tags",
+    render: (tags: string[]) => {
+      return tags.map((tag: string) => {
+        const color = "blue";
+        return (
+          <Tag color={color} key={tag}>
+            {tag.toUpperCase()}
+          </Tag>
+        );
+      });
+    },
+  },
+  {
+    title: "",
+    key: "action",
+    render: function btn() {
+      return <Button icon={<PlusOutlined />}>Add Data</Button>;
+    },
+  },
+];
+
+const data: any = [
+  {
+    key: "1",
+    team: "BBC News",
+    dataset: "Instagram",
+    lastUpdated: "12/12/2020",
+    tags: ["news", "social media", "instagram"],
+  },
+  {
+    key: "2",
+    team: "BBC News",
+    dataset: "12pm-4pm",
+    lastUpdated: "12/12/2020",
+    tags: ["news", "television", "afternoon"],
+  },
+  {
+    key: "3",
+    team: "BBC News",
+    lastUpdated: "12/12/2020",
+    dataset: "Breakfast Hour",
+    tags: ["news", "breakfast"],
+  },
+];
+
+const Home = () => {
+  return (
+    <div>
+      <SearchAutoComplete
+        dataSource={data.map(
+          (i: { team: string; dataset: string }) => `${i.team} - ${i.dataset}`
+        )}
+      />
+      <DatasetsTable data={data} columns={columns} />
+    </div>
+  );
+};
+
+export { Home };
