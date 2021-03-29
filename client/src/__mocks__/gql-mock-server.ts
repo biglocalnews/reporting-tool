6314757819dd920aa3f44e5cf35fb21813904b1f
Ro <roen@stanford.edu>
2021-03-29 12:22:19 -0400

    chore(ch112): Add mocks directory for mock gql

---
+++ client/src/__mocks__/gql-mock-server.ts
@@ -0,0 +1,35 @@
+import { ApolloServer, gql } from "apollo-server";
+import { schema } from "./schema"
+import fake from "casual";
+
+const mockTypes = {
+  User: () => ({
+    firstName: fake.first_name,
+    lastName: fake.last_name
+  }),
+  Dataset: () => (
+    {
+      name: fake.title
+    }
+  ),
+  Tag: () => (
+    {
+      name: fake.word
+    }
+  ),
+  Program: () => ({
+    name: "BBC News"
+  })
+}
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  mocks: mockTypes,
+  // mocks: true,
+  // resolvers: resolvers,
+  playground: true
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
