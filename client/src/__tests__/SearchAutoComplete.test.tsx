10c61e8f7a5eed2fb94986df604886a5f647e0ff
Ro <roen@stanford.edu>
2021-03-30 11:35:28 -0400

    test(ch112): ignore empty block for i18n mock

--- client/src/__tests__/SearchAutoComplete.test.tsx
+++ client/src/__tests__/SearchAutoComplete.test.tsx
@@ -1,25 +1,25 @@
-import { shallow } from "enzyme";
 import { render } from "@testing-library/react";
 import { SearchAutoComplete } from "../components/SearchAutoComplete";
 import { axe } from "jest-axe";
 
-jest.mock('react-i18next', () => ({
-    // this mock makes sure any components using the translate 
-    // hook can use it without a warning being shown
-    useTranslation: () => {
-        return {
-            t: (str: string) => str,
-            i18n: {
-                changeLanguage: () => new Promise(() => { }),
-            },
-        };
-    },
+jest.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate
+  // hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        /* tslint:disable:no-empty */
+        changeLanguage: () => new Promise(() => { }),
+      },
+    };
+  },
 }));
 
 describe("accessibility", () => {
-    it("should not have basic accessibility issues", async () => {
-        const { container } = render(<SearchAutoComplete dataSource={[]} />);
-        const results = await axe(container);
-        expect(results).toHaveNoViolations();
-    });
+  it("should not have basic accessibility issues", async () => {
+    const { container } = render(<SearchAutoComplete dataSource={[]} />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
