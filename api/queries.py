3ae1d2fb8d49ca27b9280bd3c6cb60cbee21c72c
nmanduj1 <nmandujano1@gmail.com>
2021-04-15 10:38:07 -0400

    DRY documentation

--- api/queries.py
+++ api/queries.py
@@ -2,14 +2,17 @@ from ariadne import convert_kwargs_to_snake_case, ObjectType
 
 query = ObjectType("Query")
 
+'''GraphQL query to find a user based on user ID.
+        :param obj: obj is a value returned by a parent resolver
+        :param info: Has context attribute that contains ContextValue specific to the server implementation.
+'''
+
 @query.field("user")
 @convert_kwargs_to_snake_case
 def resolve_user(obj, info, id):
     '''GraphQL query to find a user based on user ID.
-        :param obj: obj is a value returned by a parent resolver
-        :param info: Has context attribute that contains ContextValue specific to the server implementation.
         :param id: Id for the user to be fetched
-        :returns: 
+        :returns: User dictionary
     '''
     payload = {
         'id': 1,
