f643b314bbeec8e45100711545dacfbe0da232ad
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-04-22 14:03:16 -0400

    fix(ch174): test is fixed!

--- client/src/__tests__/DatasetDetails.test.tsx
+++ client/src/__tests__/DatasetDetails.test.tsx
@@ -4,15 +4,12 @@ import React from "react";
 import { DatasetDetails } from "../components/DatasetDetails/DatasetDetails";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
-import {
-  deleteRecordMutationFixture,
-  getDatasetQueryFixture,
-} from "../__mocks__/mockData";
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import userEvent from "@testing-library/user-event";
+import { AutoMockedProvider } from "../__mocks__/AutoMockProvider";
 
-const mocks = [getDatasetQueryFixture, deleteRecordMutationFixture];
+const history = createMemoryHistory();
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
@@ -21,103 +18,103 @@ jest.mock("react-router-dom", () => ({
     .mockReturnValue({ datasetId: "5a8ee1d5-2b5a-49db-b466-68fe50a27cdb" }),
 }));
 
+i18next.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: "en-gb",
+  interpolation: {
+    escapeValue: false,
+  },
+  keySeparator: false,
+  resources: {
+    "en-gb": {
+      translation: {
+        addData: "Add Data",
+      },
+    },
+    en: {
+      addData: "Add Data",
+    },
+  },
+  lowerCaseLng: true,
+});
+
 async function wait(ms = 0) {
-  await act(() => {
-    return new Promise((resolve) => {
+  await act(async () => {
+    return await new Promise((resolve) => {
       setTimeout(resolve, ms);
     });
   });
 }
 
-describe("Dataset Details component", () => {
-  beforeAll(() => {
-    // Mock react-i18next
-    i18next.use(initReactI18next).init({
-      lng: "en",
-      fallbackLng: "en-gb",
-      interpolation: {
-        escapeValue: false,
-      },
-      keySeparator: false,
-      resources: {
-        "en-gb": {
-          translation: {
-            addData: "Add Data",
-          },
-        },
-        en: {
-          addData: "Add Data",
-        },
-      },
-      lowerCaseLng: true,
+test("gets dataset records when component is mounted and displays them in a table", async () => {
+  render(
+    <AutoMockedProvider>
+      <Router history={history}>
+        <DatasetDetails />
+      </Router>
+    </AutoMockedProvider>
+  );
+
+  expect(screen).toBeTruthy();
+  expect(screen.getByText("Loading")).toBeInTheDocument();
+
+  await wait();
+
+  expect(screen.getByText("BBC News")).toBeInTheDocument();
+  expect(screen.getByRole("table"));
+
+  expect(screen.getAllByRole("row")).toHaveLength(1);
+});
+
+test("clicking on the add data button routes to the data entry page", async () => {
+  render(
+    <AutoMockedProvider>
+      <Router history={history}>
+        <DatasetDetails />
+      </Router>
+    </AutoMockedProvider>
+  );
+
+  await wait();
+
+  const addDataButton = screen.getByRole("button", { name: "plus Add Data" });
+  userEvent.click(addDataButton);
+
+  expect(history.location.pathname).toBe(
+    "/dataset/5a8ee1d5-2b5a-49db-b466-68fe50a27cdb/entry"
+  );
+});
+
+test("should delete table from record when delete button is clicked", async () => {
+  const { container } = render(
+    <AutoMockedProvider>
+      <Router history={history}>
+        <DatasetDetails />
+      </Router>
+    </AutoMockedProvider>
+  );
+
+  await wait();
+
+  await act(async () => {
+    const recordToDelete = container.querySelector(
+      "tr[data-row-key='05caae8d-bb1a-416e-9dda-bb251fe474ff']"
+    ) as HTMLElement;
+
+    const deleteButtonforRecord = within(recordToDelete).getByRole("button", {
+      name: "Delete",
     });
-  });
-
-  const history = createMemoryHistory();
-
-  test("gets dataset records when component is mounted and displays them in a table", async () => {
-    render(
-      <MockedProvider mocks={mocks}>
-        <Router history={history}>
-          <DatasetDetails />
-        </Router>
-      </MockedProvider>
-    );
-
-    expect(screen).toBeTruthy();
-    expect(screen.getByText("Loading")).toBeInTheDocument();
+    userEvent.click(deleteButtonforRecord);
 
     await wait();
-
-    expect(screen.getByText("BBC News")).toBeInTheDocument();
-    expect(screen.getByRole("table"));
-    expect(screen.getAllByRole("row")).toHaveLength(1);
-  });
-
-  test("clicking on the add data button routes to the data entry page", async () => {
-    render(
-      <MockedProvider mocks={mocks}>
-        <Router history={history}>
-          <DatasetDetails />
-        </Router>
-      </MockedProvider>
-    );
-
-    await wait();
-
-    const addDataButton = screen.getByRole("button", { name: "plus Add Data" });
-    userEvent.click(addDataButton);
-
-    expect(history.location.pathname).toBe(
-      "/dataset/5a8ee1d5-2b5a-49db-b466-68fe50a27cdb/entry"
-    );
-  });
-
-  test("should delete table from record when delete button is clicked", async () => {
-    render(
-      <MockedProvider mocks={mocks}>
-        <Router history={history}>
-          <DatasetDetails />
-        </Router>
-      </MockedProvider>
-    );
-
-    await wait();
-
-    const firstRowOfRecords = screen.getAllByRole("row")[0];
-    userEvent.click(
-      within(firstRowOfRecords).getByRole("button", { name: "Delete" })
-    );
 
     const popConfirmDeleteAction = screen.getByRole("tooltip");
-    userEvent.click(
+    fireEvent.click(
       within(popConfirmDeleteAction).getByRole("button", {
         name: "Yes, delete",
       })
     );
-
-    await wait();
-
-    expect(screen.getAllByRole("row")).toHaveLength(0);
   });
+
+  expect(screen.getByText("No Data")).toBeInTheDocument();
 });
