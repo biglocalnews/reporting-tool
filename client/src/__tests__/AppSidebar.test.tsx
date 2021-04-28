0df34ffc9433506ad8266f94dab88cfaef05ebc6
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-04-28 15:18:03 -0400

    test(ch168): fix failed import in test

--- client/src/__tests__/AppSidebar.test.tsx
+++ client/src/__tests__/AppSidebar.test.tsx
@@ -8,7 +8,7 @@ import { MockedProvider } from "@apollo/client/testing";
 import { act, render, wait, waitFor } from "@testing-library/react";
 import { gql } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
-import { GET_USER } from "../__queries__/getUser.gql";
+import { GET_USER } from "../__queries__/GetUser.gql";
 
 describe("UK English internationalization", () => {
   beforeAll(() => {
