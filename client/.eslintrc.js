183c889c3f59704286698da52a06582ecc5e5887
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-05-14 20:56:52 -0400

    chore: update eslint config to use typescript-eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,27 +1,29 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
-    },
+      jsx: true // Allows for the parsing of JSX
+    }
   },
   settings: {
     react: {
-      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
+      version: "detect"
+    }
   },
   extends: [
+    "eslint:recommended", // Turns on a small, sensible set of rules which lint for well-known best-practices
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    "plugin:jsx-a11y/recommended"
+    "plugin:jsx-a11y/strict", // Enforce strict accessibility rules
+    "prettier" // Turns off all rules that are unnecessary or might conflict with Prettier
   ],
-  plugins: ["jsx-a11y", "react-hooks"],
+  plugins: ["react-hooks"],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
-  },
+  }
 };
