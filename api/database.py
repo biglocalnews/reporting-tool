48a3c2f7dbe0ffa8aef2914d5d54f41f509e5b3c
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-06-03 11:12:38 -0400

    chore(ch240): add dummy data for disability

--- api/database.py
+++ api/database.py
@@ -303,6 +303,7 @@ def create_dummy_data(session):
     tag.datasets.append(ds1)
     tag.datasets.append(ds2)
 
+    # Gender category
     category_non_binary = Category(id='51349e29-290e-4398-a401-5bf7d04af75e', description='Non-binary gender',
                                     category='gender', category_value='non-binary')
     category_cis_women = Category(id='0034d015-0652-497d-ab4a-d42b0bdf08cb', description='Cisgender women gender',
@@ -316,19 +317,30 @@ def create_dummy_data(session):
     category_gender_non_conforming = Category(id='a72ced2b-b1a6-4d3d-b003-e35e980960df', description='Gender non-conforming gender',
                                     category='gender', category_value='gender non-conforming')
 
+    # Disability category
+    category_disability_1 = Category(id='c36958cb-cc62-479e-ab61-eb03896a981c', description='disability description',
+                                    category='disability', category_value='disability')
+    category_disability_2 = Category(id='55119215-71e9-43ca-b2c1-7e7fb8cec2fd', description='disability description',
+                                    category='disability', category_value='no disability')
+
+    # Targets 
     target_non_binary = Target(id='40eaeafc-3311-4294-a639-a826eb6495ab', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='51349e29-290e-4398-a401-5bf7d04af75e', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.16666666666))
     target_cis_women = Target(id='eccf90e8-3261-46c1-acd5-507f9113ff72', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='0034d015-0652-497d-ab4a-d42b0bdf08cb', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.16666666666))
     target_cis_men = Target(id='2d501688-92e3-455e-9685-01141de3dbaf', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='d237a422-5858-459c-bd01-a0abdc077e5b', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.16666666666))
     target_trans_women = Target(id='4f7897c2-32a1-4b1e-9749-1a8066faca01', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='662557e5-aca8-4cec-ad72-119ad9cda81b', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.16666666666))
     target_trans_men = Target(id='9352b16b-2607-4f7d-a272-fe6dedd8165a', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='1525cce8-7db3-4e73-b5b0-d2bd14777534', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.16666666666))
     target_gender_non_conforming = Target(id='a459ed7f-5573-4d5b-ade6-3070bc8bd2db', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='a72ced2b-b1a6-4d3d-b003-e35e980960df', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.16666666666))
-          
+    target_disability_1 = Target(id='b5be10ce-103f-41f2-b4c4-603228724993', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='c36958cb-cc62-479e-ab61-eb03896a981c', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.50))
+    target_disability_2 = Target(id='6e6edce5-3d24-4296-b929-5eec26d52afc', program_id='1e73e788-0808-4ee8-9b25-682b6fa3868b', category_id='55119215-71e9-43ca-b2c1-7e7fb8cec2fd', target_date=datetime.strptime('2022-12-31 00:00:00', '%Y-%m-%d %H:%M:%S'), target=float(.50))
+
     category_non_binary.targets.append(target_non_binary)
     category_cis_women.targets.append(target_cis_women)
     category_cis_men.targets.append(target_cis_men)
     category_trans_women.targets.append(target_trans_women)
     category_trans_men.targets.append(target_trans_men)
     category_gender_non_conforming.targets.append(target_gender_non_conforming)
+    category_disability_1.targets.append(target_disability_1)
+    category_disability_2.targets.append(target_disability_2)
 
     session.add(category_non_binary)
     session.add(category_cis_women)
@@ -336,6 +348,8 @@ def create_dummy_data(session):
     session.add(category_trans_women)
     session.add(category_trans_men)
     session.add(category_gender_non_conforming)
+    session.add(category_disability_1)
+    session.add(category_disability_2)
 
     # datetime.strptime converts a string to a datetime object bc of SQLite DateTime limitation- must be explicit about format
     record = Record(id='742b5971-eeb6-4f7a-8275-6111f2342bb4', dataset_id='b3e7d42d-2bb7-4e25-a4e1-b8d30f3f6e89', publication_date=datetime.strptime('2020-12-21 00:00:00', '%Y-%m-%d %H:%M:%S')) 
@@ -347,6 +361,8 @@ def create_dummy_data(session):
     entry4 = Entry(id='407f24d0-c5eb-4297-9495-90e325a00a1d', count=1) 
     entry5 = Entry(id='4adcb9f9-c1eb-41ba-b9aa-ed0947311a24', count=1) 
     entry6 = Entry(id='1c49c64f-51e6-48fe-af10-69aaeeddc55f', count=1) 
+    entry7 = Entry(id='335b3680-13a1-4d8f-a917-01e1e7e1311a', count=1)
+    entry8 = Entry(id='fa5f1f0e-d5ba-4f2d-bdbf-819470a6fa4a', count=1)
     
     category_non_binary.entries.append(entry1)
     category_cis_women.entries.append(entry2)
@@ -354,6 +370,8 @@ def create_dummy_data(session):
     category_trans_women.entries.append(entry4)
     category_trans_men.entries.append(entry5)
     category_gender_non_conforming.entries.append(entry6)
+    category_disability_1.entries.append(entry7)
+    category_disability_2.entries.append(entry8)
 
     record.entries.append(entry1)
     record.entries.append(entry2)
@@ -361,6 +379,8 @@ def create_dummy_data(session):
     record.entries.append(entry4)
     record.entries.append(entry5)
     record.entries.append(entry6)
+    record.entries.append(entry7)
+    record.entries.append(entry8)
 
     session.add(org)
     session.commit()
