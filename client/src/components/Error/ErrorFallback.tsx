47c34aaf371fc30bce00deac46d4f6398a6e0de2
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-06-17 01:19:04 -0400

    feat: add error component - used in home page

---
+++ client/src/components/Error/ErrorFallback.tsx
@@ -0,0 +1,21 @@
+import { ApolloError } from "apollo-server";
+import React from "react";
+import { Alert } from "antd";
+import "./ErrorFallback.css";
+
+interface ErrorProps {
+  error: Error | ApolloError;
+}
+
+export const ErrorFallback = ({ error }: ErrorProps): JSX.Element => {
+  return (
+    <Alert
+      className="error-alert"
+      role="alert"
+      message="Something went wrong"
+      description={`${error.message} Please refresh and try again.`}
+      type="error"
+      showIcon
+    />
+  );
+};
