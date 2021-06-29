1fbb9484a08da8999352f3e1c8b15dd394ea2de1
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-06-29 10:24:15 -0400

    refactor: move apollo hooks to component file

---
+++ client/src/components/DataEntry/hooks.ts
@@ -0,0 +1,66 @@
+import { useMutation } from "@apollo/client";
+import { CREATE_RECORD } from "../../__mutations__/CreateRecord";
+import { GET_DATASET } from "../../__queries__/GetDataset.gql";
+import { UPDATE_RECORD } from "../../__mutations__/UpdateRecord.gql";
+
+interface CreateRecordMutationProps {
+  datasetId: string;
+}
+
+export const useCreateRecordMutation = ({
+  datasetId,
+}: CreateRecordMutationProps) => {
+  const [
+    createRecord,
+    {
+      data: newRecordData,
+      loading: loadingRecordCreation,
+      error: errorOnCreate,
+    },
+  ] = useMutation(CREATE_RECORD, {
+    awaitRefetchQueries: true,
+    refetchQueries: [
+      {
+        query: GET_DATASET,
+        variables: { id: datasetId },
+      },
+    ],
+  });
+  return { createRecord, newRecordData, loadingRecordCreation, errorOnCreate };
+};
+
+interface UpdateRecordMutationProps {
+  datasetId: string;
+  // formSuccess: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const useUpdateRecordMutation = ({
+  datasetId,
+}: UpdateRecordMutationProps) => {
+  const [
+    updateRecord,
+    {
+      data: updatedRecordData,
+      loading: loadingRecordUpdate,
+      error: errorOnRecordUpdate,
+    },
+  ] = useMutation(UPDATE_RECORD, {
+    // onCompleted: (data) => {
+    //   // if (data) formSuccess(true);
+    // },
+    awaitRefetchQueries: true,
+    refetchQueries: [
+      {
+        query: GET_DATASET,
+        variables: { id: datasetId },
+      },
+    ],
+  });
+
+  return {
+    updateRecord,
+    updatedRecordData,
+    loadingRecordUpdate,
+    errorOnRecordUpdate,
+  };
+};
