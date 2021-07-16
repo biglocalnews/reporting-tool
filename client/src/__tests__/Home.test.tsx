c4b0fc273e6ef42fdc83b53af41adc5b0cff0326
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-07-15 22:30:58 -0400

    refactor: rename no data on user home page

--- client/src/__tests__/Home.test.tsx
+++ client/src/__tests__/Home.test.tsx
@@ -55,8 +55,8 @@ test("should render home page datasets and formatted 'last updated' date", async
   expect(within(row).getAllByRole("cell")[2].textContent).toBe("May 10, 2021");
 });
 
-test("should render No Data Available for 'last updated' date when no records exist", async () => {
-  const { auth } = mockUserLoggedIn();
+test("should render No Data Available Yet for 'last updated' date when no records exist", async () => {
+  const { auth, mock } = mockUserLoggedIn();
   await auth.init();
 
   const mockDateTime = {
@@ -82,7 +82,7 @@ test("should render No Data Available for 'last updated' date when no records ex
   const row = screen.getAllByRole("row")[1];
   expect(row).toHaveTextContent(/breakfast hour/i);
   expect(within(row).getAllByRole("cell")[2].textContent).toBe(
-    "No Data Available"
+    "No Data Available Yet"
   );
 });
 
