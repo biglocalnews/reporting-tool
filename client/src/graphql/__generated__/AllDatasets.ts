7bac07d819348a85f2dd0b64a4abe112ff02409a
Joe Nudell <jnu@stanford.edu>
2021-07-22 00:01:46 -0700

    pull all datasets for admin

---
+++ client/src/graphql/__generated__/AllDatasets.ts
@@ -0,0 +1,45 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: AllDatasets
+// ====================================================
+
+export interface AllDatasets_teams_programs_datasets_records {
+  readonly __typename: "Record";
+  readonly id: string;
+  readonly publicationDate: any;
+}
+
+export interface AllDatasets_teams_programs_datasets_tags {
+  readonly __typename: "Tag";
+  readonly name: string;
+}
+
+export interface AllDatasets_teams_programs_datasets {
+  readonly __typename: "Dataset";
+  readonly id: string;
+  readonly name: string;
+  readonly description: string | null;
+  readonly lastUpdated: any | null;
+  readonly records: ReadonlyArray<AllDatasets_teams_programs_datasets_records>;
+  readonly tags: ReadonlyArray<AllDatasets_teams_programs_datasets_tags>;
+}
+
+export interface AllDatasets_teams_programs {
+  readonly __typename: "Program";
+  readonly id: string;
+  readonly name: string;
+  readonly datasets: ReadonlyArray<AllDatasets_teams_programs_datasets>;
+}
+
+export interface AllDatasets_teams {
+  readonly __typename: "Team";
+  readonly programs: ReadonlyArray<AllDatasets_teams_programs>;
+}
+
+export interface AllDatasets {
+  readonly teams: ReadonlyArray<AllDatasets_teams>;
+}
