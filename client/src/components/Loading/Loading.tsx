66e736032a40fb302818533abca8fb6c5b7f3cdf
Joe Nudell <jnu@stanford.edu>
2021-07-23 14:56:13 -0700

    a11y for loading spinner

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -1,11 +1,22 @@
 import { Spin } from "antd";
+import { useTranslation } from "react-i18next";
 import "./Loading.css";
 
 /**
  * Full-screen loading spinner.
  */
-export const Loading = () => (
-  <div id="loading-container">
-    <Spin size="large" />
-  </div>
-);
+export const Loading = () => {
+  const { t } = useTranslation();
+  return (
+    <div
+      id="loading-container"
+      aria-hidden="false"
+      aria-valuetext={t("loading")}
+      aria-busy="true"
+      role="progressbar"
+      aria-label={t("loading")}
+    >
+      <Spin size="large" />
+    </div>
+  );
+};
