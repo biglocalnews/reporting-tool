9e5779776c317b6160456a529e1d7954d5c3d671
Robert Cobain <robert.cobain@bbc.co.uk>
2021-07-28 14:53:52 +0100

    bbc stuff

--- db_init.sh
+++ db_init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it `docker ps --format '{{.ID}}' --filter label=com.docker.swarm.service.name=5050-api` sh -c 'psql -h 5050-db --user postgres -c "create database rt" && python3 /app/api/database.py --dummy-data'
+docker exec -it `docker ps --format '{{.ID}}' --filter label=com.docker.swarm.service.name=5050-api` sh -c 'psql -h 5050-db --user postgres -c "create database rt"; python3 /app/database.py --dummy-data'
