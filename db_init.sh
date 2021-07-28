3fa082100a2b0dbb5643cae65a8e4ed923676848
Robert Cobain <robert.cobain@bbc.co.uk>
2021-07-28 17:49:55 +0100

    test actions

--- db_init.sh
+++ db_init.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+docker service update --detach --constraint-add no_such_node==true 5050-db
+sleep 5
+
+rm -rf /mnt/data/postgres/5050
+mkdir /mnt/data/postgres/5050
+
+docker service update --constraint-rm no_such_node==true 5050-db
+
 docker exec -it `docker ps --format '{{.ID}}' --filter label=com.docker.swarm.service.name=5050-api` sh -c 'psql -h 5050-db --user postgres -c "create database rt"; python3 /app/database.py --dummy-data'
--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,7 @@ docker push 5050.ni.bbc.co.uk:8443/5050-api
 docker build -f ./client/bbc.dockerfile -t 5050.ni.bbc.co.uk:8443/5050-client ./client
 docker push 5050.ni.bbc.co.uk:8443/5050-client
 
-docker service update --detach --constraint-add no_such_node==true 5050-db
-sleep 5
-sudo rm -rf /mnt/data/postgres/5050
-sudo mkdir /mnt/data/postgres/5050
-docker service update --constraint-rm no_such_node==true 5050-db
-
 docker service update --force --image 5050.ni.bbc.co.uk:8443/5050-api:latest 5050-api
 docker service update --force --image 5050.ni.bbc.co.uk:8443/5050-client:latest 5050-client-prod
 
-./db_init.sh
+#./db_init.sh
