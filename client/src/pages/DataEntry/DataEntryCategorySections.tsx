a477ab0c87d912b4cc3a89f9660db20c8ebd7ef9
gavinwright292 <48646302+gavinwright292@users.noreply.github.com>
2021-07-29 07:34:58 +0100

    remove category descriptions from DataEntryCategorySections.tsx

--- client/src/pages/DataEntry/DataEntryCategorySections.tsx
+++ client/src/pages/DataEntry/DataEntryCategorySections.tsx
@@ -128,15 +128,8 @@ const DataEntryCategorySections = ({
             gutter={[16, 16]}
             className="data-entry"
           >
-            <Col span={7}>
-              <h3 className="data-entry_category-descr-header">
-                {t("aboutAttribute", { attribute: category.categoryName })}
-              </h3>
-              {t("attributeDescription", {
-                description: category.description,
-              })}
-            </Col>
-            <Col span={17}>
+
+            <Col span={24}>
               <Card
                 key={category.categoryName}
                 type="inner"
