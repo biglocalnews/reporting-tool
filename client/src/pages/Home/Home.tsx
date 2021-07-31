9242e067200fa9c07a599698237da1eb30318030
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-07-31 01:32:19 -0400

    feat(ch261): add team name filter and alert on filter

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from "@apollo/client";
+import { Alert, Button } from "antd";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TFunction, useTranslation } from "react-i18next";
+import { useHistory, useLocation } from "react-router-dom";
 import { useAuth } from "../../components/AuthProvider";
 import { ErrorFallback } from "../../components/Error/ErrorFallback";
 import {
@@ -25,6 +27,13 @@ export interface TableData {
   tags: Array<string>;
   [key: string]: string | string[];
 }
+
+/**
+ * URL parameters expected for searching table
+ */
+type HomeRouteParams = Readonly<{
+  programTeamSearchTerm: string;
+}>;
 
 const getTableData = (
   queryData: AllDatasets_teams[],
@@ -58,6 +67,8 @@ const Home = (): JSX.Element => {
   const auth = useAuth();
   const userId = auth.getUserId();
 
+  const history = useHistory();
+
   const { data, loading, error } = useQuery<GetUser, GetUserVariables>(
     GET_USER,
     {
@@ -68,10 +79,9 @@ const Home = (): JSX.Element => {
     skip: !auth.isAdmin(),
   });
 
-  const [filteredData, setFilteredData] = useState<Array<TableData>>([]);
-
   const originalTeamData = allTeams?.data?.teams || data?.user?.teams || [];
   const rowData = getTableData(originalTeamData.slice(), t);
+  const [filteredData, setFilteredData] = useState<Array<TableData>>(rowData);
 
   // Filters datasets table by search term
   const handleTableSearchFilter = (searchText: string) => {
@@ -85,25 +95,54 @@ const Home = (): JSX.Element => {
     setFilteredData(filteredData);
   };
 
+  const location = useLocation<HomeRouteParams>();
+  const teamNameFilter = location.search.substring(1);
+  useEffect(() => {
+    if (teamNameFilter) {
+      handleTableSearchFilter(teamNameFilter);
+    } else {
+      setFilteredData(rowData);
+    }
+  }, [teamNameFilter]);
+
   if (error) return <ErrorFallback error={error} />;
 
   return (
     <>
-      <div
-        id="home_table-search"
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          marginBottom: "1rem",
-        }}
-      >
-        <HomeSearchAutoComplete onSearch={handleTableSearchFilter} />
-      </div>
+      {teamNameFilter ? (
+        <Alert
+          style={{ margin: "1rem 0rem" }}
+          message={`Showing datasets for team: ${teamNameFilter.toUpperCase()}`}
+          type="info"
+          action={
+            <Button
+              style={{ margin: ".5rem 0rem" }}
+              size="small"
+              type="primary"
+              onClick={() => history.push("/")}
+            >
+              Show All My Teams
+            </Button>
+          }
+        />
+      ) : (
+        <div
+          id="home_table-search"
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            marginBottom: "1rem",
+          }}
+        >
+          <HomeSearchAutoComplete onSearch={handleTableSearchFilter} />
+        </div>
+      )}
 
       <HomeDatasetsListTable
         loading={loading}
         filteredData={filteredData}
         rowData={rowData}
+        teamNameFilterText={teamNameFilter}
       />
     </>
   );
