33a0327d603fc89ef6b91c8d3167f97391a1f896
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-11-10 11:07:42 -0500

    feat: add date range to category queries

--- api/reporting_queries.py
+++ api/reporting_queries.py
@@ -1,6 +1,8 @@
+from typing import Any
 from ariadne import convert_kwargs_to_snake_case, ObjectType
+from ariadne.types import GraphQLResolveInfo
 from sqlalchemy.sql.functions import func
-from database import (CategoryValue, Entry)
+from database import (CategoryValue, Entry, Record)
 from queries import resolve_category, resolve_categories
 
 query = ObjectType("Query")
@@ -10,28 +12,42 @@ reporting_queries = [query, category_overview]
 
 @query.field("categoryOverview")
 @convert_kwargs_to_snake_case
-def resolve_category_overview(obj, info, id):
+def resolve_category_overview(obj: Any, info: GraphQLResolveInfo, id, **kwargs):
     '''GraphQL query to fetch category by ID for overview.
+    :param id: ID of category to retrieve
+    :param kwargs: Optional parameters passed to resolver
     :returns: category object
     '''
+
+    print("obj", obj)
     return resolve_category(obj, info, id)
 
 
 @category_overview.field("sumCategoryValues")
-def resolve_sum_category_values(category, info):
+@convert_kwargs_to_snake_case
+def resolve_sum_category_values(category: Any, info: GraphQLResolveInfo):
     '''GraphQL query to sum the counts by category value
         :param category: Category object to filter counts for
         :returns: Dictionary
     '''
     session = info.context['dbsession']
+
     stmt = session.query(
             CategoryValue.name.label('category_value_name'),
             CategoryValue.id.label('category_value_id'),
             func.sum(Entry.count).label('sum_of_counts')).\
-            join(Entry).\
-                group_by(CategoryValue.id).\
+                join(CategoryValue.entries).\
+                join(Entry.record).\
+                    group_by(CategoryValue.id).\
                     filter(CategoryValue.category_id == category.id, Entry.deleted == None, CategoryValue.deleted == None)
 
+    # optional start and end date params passed to categoryOverview
+    if 'dateRange' in info.variable_values:
+        start = info.variable_values['dateRange']['start_date']
+        end = info.variable_values['dateRange']['end_date']
+        # filter results by date range if provided in thr query
+        stmt = stmt.filter(Record.publication_date >= start, Record.publication_date <= end)
+                                
     return [
         {'category_value_id': row.category_value_id,
          'category_value': row.category_value_name,
@@ -41,8 +57,8 @@ def resolve_sum_category_values(category, info):
 
 @query.field("categoriesOverview")
 @convert_kwargs_to_snake_case
-def resolve_categories_overview(obj, info):
-    '''GraphQL query to fetch overview for all categories.
+def resolve_categories_overview(obj: Any, info: GraphQLResolveInfo):
+    '''GraphQL query to fetch overview for all categories
     :returns: category object
     '''
     return resolve_categories(obj, info)
