244452abb2089078b8413eec0502f48dba6c11c5
Ro E <10480484+seerocode@users.noreply.github.com>
2021-11-15 12:03:13 -0500

    [SC359] Add queries for reporting category overview (#117)
    * feat: add schema for reporting queries
    
    * feat: add resolvers for reporting queries
    
    * feat: add permissions for admin to queries
    
    * refactor: move sum of category values to Category db class method
    
    * refactor: remove second query from category overview resolver
    
    * style(sc361): clean up imports
    
    * style(sc361): remove unused imports
    
    * refactor: move query out of category class
    
    * feat: add query for all category overview
    
    * feat: add date range to category queries
    
    * fix: add missing kwargs
    
    * style(sc361): remove print statement
    
    * refactor: avoid loading entire category object
    
    * feat: add counts for tables
    
    * feat(sc361): add reporting overview main query with counts
    
    * fix: snake case to camel case
    
    * style: remove comment
    
    * fix: remove team member permission

---
+++ api/reporting_queries.py
@@ -0,0 +1,91 @@
+from typing import Any
+from ariadne import convert_kwargs_to_snake_case, ObjectType
+from ariadne.types import GraphQLResolveInfo
+from sqlalchemy.sql.functions import func
+from database import CategoryValue, Entry, Record, Category, Dataset, Tag, Team
+
+query = ObjectType("Query")
+category_overview = ObjectType("CategoryOverview")
+reporting_overview = ObjectType("ReportingOverview")
+
+reporting_queries = [query, category_overview, reporting_overview]
+
+
+@query.field("reportingOverview")
+@reporting_overview.field("categoriesOverview")
+@convert_kwargs_to_snake_case
+def resolve_reporting_overview(obj: Any, info: GraphQLResolveInfo, **kwargs):
+    '''GraphQL query to fetch fields for reporting overview.
+    :returns: reporting overview object fields
+    '''
+    categories_overview = resolve_categories_overview(obj, info, **kwargs)
+    return categories_overview
+
+@reporting_overview.field("countData")
+def resolve_count_data(obj: Any, info: GraphQLResolveInfo, **kwargs):
+    '''GraphQL query to fetch counts for teams, datasets, and tags
+    :returns: dict of counts for teams, datasets, and tags
+    '''
+    session = info.context['dbsession']
+
+    teams_count = Team.get_unfiltered_count(session)
+    datasets_count = Dataset.get_unfiltered_count(session)
+    tags_count = Tag.get_unfiltered_count(session)
+
+    return {'teams_count': teams_count, 'datasets_count': datasets_count, 'tags_count': tags_count}
+
+
+@query.field("categoryOverview")
+@convert_kwargs_to_snake_case
+def resolve_category_overview(obj: Any, info: GraphQLResolveInfo, id, **kwargs):
+    '''GraphQL query to fetch category by ID for overview.
+    :param id: ID of category to retrieve
+    :param kwargs: Optional parameters passed to resolver
+    :returns: category object
+    '''
+    session = info.context['dbsession']
+    return session.query().with_entities(Category.id, Category.name, Category.description).\
+        filter(Category.id == id, Category.deleted == None).first()
+
+
+@category_overview.field("sumCategoryValues")
+@convert_kwargs_to_snake_case
+def resolve_sum_category_values(category: Any, info: GraphQLResolveInfo):
+    '''GraphQL query to sum the counts by category value
+        :param category: Category object to filter counts for
+        :returns: Dictionary
+    '''
+    session = info.context['dbsession']
+
+    stmt = session.query(
+            CategoryValue.name.label('category_value_name'),
+            CategoryValue.id.label('category_value_id'),
+            func.sum(Entry.count).label('sum_of_counts')).\
+                join(CategoryValue.entries).\
+                join(Entry.record).\
+                    group_by(CategoryValue.id).\
+                    filter(CategoryValue.category_id == category.id, Entry.deleted == None, CategoryValue.deleted == None)
+
+    # optional start and end date params passed to categoryOverview
+    if 'dateRange' in info.variable_values:
+        start = info.variable_values['dateRange']['startDate']
+        end = info.variable_values['dateRange']['endDate']
+        # filter results by date range if provided in thr query
+        stmt = stmt.filter(Record.publication_date >= start, Record.publication_date <= end)
+                                
+    return [
+        {'category_value_id': row.category_value_id,
+         'category_value': row.category_value_name,
+         'sum': row.sum_of_counts}
+        for row in stmt]
+
+
+@query.field("categoriesOverview")
+@convert_kwargs_to_snake_case
+def resolve_categories_overview(obj: Any, info: GraphQLResolveInfo, **kwargs):
+    '''GraphQL query to fetch overview for all categories
+    :returns: category object
+    '''
+    session = info.context['dbsession']
+    return session.query().with_entities(Category.id, Category.name, Category.description).\
+        filter(Category.deleted == None).all()
