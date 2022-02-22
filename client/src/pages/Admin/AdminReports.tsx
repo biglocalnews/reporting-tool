3ba1c729af43391cdd574a261d7352a49111cc0b
Robert Cobain <robert.cobain@bbc.co.uk>
2022-02-22 14:25:11 +0000

    Admin stats WIP. Use table

--- client/src/pages/Admin/AdminReports.tsx
+++ client/src/pages/Admin/AdminReports.tsx
@@ -1,41 +1,38 @@
 import { useQuery } from "@apollo/client";
-import { Button, Card, Col, Divider, List, PageHeader, Row, Select, Space, Statistic } from "antd";
+import { Button, Card, Col, Divider, List, PageHeader, Row, Select, Space, Statistic, Table } from "antd";
 import { WomanOutlined, IdcardOutlined, EyeInvisibleOutlined, MailOutlined, AlertOutlined } from '@ant-design/icons';
 import { useTranslation } from "react-i18next";
 import { GetAdminStats } from "../../graphql/__generated__/GetAdminStats";
 import { AdminStatsInput, TargetStateType } from "../../graphql/__generated__/globalTypes";
 import { GET_ADMIN_STATS } from "../../graphql/__queries__/GetAdminStats";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { cardStyle } from "../Home/Home";
+import { SortOrder } from "antd/lib/table/interface";
+import { AlignType } from "rc-table/lib/interface";
 
-
-interface IListItem {
-    datasetId: string,
-    name: string,
-    reportingPeriodEnd: Date
-}
-
-interface ITargetItem extends IListItem {
-    category: string,
-    percent: number
-}
-
-interface IOverdueItem extends IListItem {
-    reportingPeriodName: string | null | undefined
-}
 
 interface IDatasetList {
     datasetId: string,
     name: string,
     reportingPeriodEnd: Date,
     key: string
-    render(): JSX.Element
+    category?: string,
+    percent?: number,
+    reportingPeriodName?: string | null | undefined
+}
+
+const selectedCardStyle = {
+    ...cardStyle,
+    border: "2px solid green",
+    boxShadow: "0.2em 0.3em 0.75em rgba(0,50,0,0.3)"
 }
 
 export const AdminReports = () => {
 
     const [filterState, setFilterState] = useState<AdminStatsInput>({ duration: 31 });
+
+    const [selectedStat, setSelectedStat] = useState("failedTarget");
 
     const { data: adminStats, loading } = useQuery<GetAdminStats>(GET_ADMIN_STATS,
         {
@@ -58,8 +55,7 @@ export const AdminReports = () => {
             name: x.name,
             reportingPeriodEnd: x.reportingPeriodEnd,
             reportingPeriodName: x.reportingPeriodName,
-            key: `${x.datasetId}-${x.reportingPeriodEnd}`,
-            render: () => renderOverdueListItem(x)
+            key: `${x.datasetId}-${x.reportingPeriodEnd}`
         }));
 
     }, [overdueDatasets]);
@@ -77,8 +73,9 @@ export const AdminReports = () => {
                 name: x.name,
                 reportingPeriodEnd: x.reportingPeriodEnd,
                 reportingPeriodName: undefined,
-                key: `${x.category}-${x.prsId}-${x.state}`,
-                render: () => renderListItem(x)
+                percent: x.percent,
+                category: x.category,
+                key: `${x.category}-${x.prsId}-${x.state}`
             }));
 
     }, [allDatasets]);
@@ -89,8 +86,9 @@ export const AdminReports = () => {
             name: x.name,
             reportingPeriodEnd: x.reportingPeriodEnd,
             reportingPeriodName: undefined,
-            key: `${x.category}-${x.prsId}-${x.state}`,
-            render: () => renderListItem(x)
+            percent: x.percent,
+            category: x.category,
+            key: `${x.category}-${x.prsId}-${x.state}`
         }));
     }, [allDatasets]);
 
@@ -104,28 +102,88 @@ export const AdminReports = () => {
 
     const { t } = useTranslation();
 
-    const renderListItem = (x: ITargetItem): JSX.Element => <>
-        {(() => {
-            switch (x.category) {
-                case "Gender":
-                    return <WomanOutlined />
-                case "Ethnicity":
-                    return <IdcardOutlined />
-                case "Disability":
-                    return <EyeInvisibleOutlined />
+
+    const dataListBaseColumns = useMemo(() => {
+        const basicColumns = [
+            {
+                title: t("admin.reports.datasetReportingPeriodEndColumnTitle"),
+                dataIndex: "reportingPeriodEnd",
+                key: "reportingPeriodEnd",
+                sorter: (a: IDatasetList, b: IDatasetList) => a.reportingPeriodEnd.valueOf() - b.reportingPeriodEnd.valueOf(),
+                sortDirections: ['ascend', 'descend'] as SortOrder[],
+                render: (rpe: Date) => new Date(rpe).toLocaleString(navigator.language, { day: "2-digit", month: "short", year: "numeric" } as Intl.DateTimeFormatOptions)
+            },
+            {
+                title: t("admin.reports.datasetNameColumnTitle"),
+                dataIndex: "name",
+                key: "name",
+                sorter: (a: IDatasetList, b: IDatasetList) => a.name.localeCompare(b.name),
+                sortDirections: ['ascend', 'descend'] as SortOrder[],
+                render: (name: string, record: IDatasetList) => <Link to={`/dataset/${record.datasetId}/details`}>{name}</Link>
             }
-        })()}{`${x.category} ${x.percent.toFixed(2)}%`}<Link to={`/dataset/${x.datasetId}/details`}>{x.name}</Link>
-    </>
+        ];
 
-    const dateFormatOptions: Intl.DateTimeFormatOptions = { day: "2-digit", month: "short", year: "numeric" };
+        const actionsColumn = {
+            title: "",
+            key: "action",
+            align: "right" as AlignType,
+            render: () => (
+                <Space>
+                    <MailOutlined /><Button type="text">{t("admin.reports.emailTeam")}</Button>
+                    <AlertOutlined /><Button type="text">{t("admin.reports.alertTeam")}</Button>
+                </Space>
+            )
+        };
 
-    const renderOverdueListItem = (x: IOverdueItem): JSX.Element => <>
+        const targetColumns = [{
+            title: t("admin.reports.datasetCategoryColumnTitle"),
+            dataIndex: "category",
+            key: "category",
+            render: (category: string, record: IDatasetList) => <Space>
+                {(() => {
+                    switch (record.category) {
+                        case "Gender":
+                            return <WomanOutlined />
+                        case "Ethnicity":
+                            return <IdcardOutlined />
+                        case "Disability":
+                            return <EyeInvisibleOutlined />
+                    }
+                })()}{`${category}`}
+            </Space>
+        },
         {
-            `Due ${new Date(x.reportingPeriodEnd).toLocaleString(navigator.language, dateFormatOptions)}`
+            title: t("admin.reports.datasetPercentColumnTitle"),
+            dataIndex: "percent",
+            key: "percent",
+            render: (percent: number | undefined) => `${percent?.toFixed(2)}%`
         }
-        <Link to={`/dataset/${x.datasetId}/details`}>{x.name}</Link>
-    </>
+        ];
 
+        if (!datasetList || !datasetList.length) return basicColumns;
+
+        return "percent" && "category" in datasetList[0] ? [...basicColumns, ...targetColumns, actionsColumn] : [...basicColumns, actionsColumn];
+
+    }, [t, datasetList]);
+
+    useEffect(() => {
+        switch (selectedStat) {
+            case "failedTarget":
+                setDatasetList(failedDatasets);
+                break;
+            case "exceededTarget":
+                setDatasetList(goodDatasets);
+                break;
+            case "overdue":
+                setDatasetList(overdueDatasetsListItems);
+                break;
+            case "needsAttention":
+                setDatasetList(overdueDatasetsListItems);
+                break;
+            default:
+                setDatasetList(undefined);
+        }
+    }, [selectedStat, setDatasetList, failedDatasets, goodDatasets, overdueDatasetsListItems]);
 
     return <>
         <PageHeader title={t("admin.reports.title")} subTitle={t("admin.reports.subtitle")} />
@@ -135,10 +193,10 @@ export const AdminReports = () => {
                     role="button"
                     tabIndex={0}
                     style={{ cursor: "pointer" }}
-                    onKeyDown={(e) => { if (e.key === 'Enter') { setDatasetList(goodDatasets) } }}
-                    onClick={() => setDatasetList(failedDatasets)}
+                    onKeyDown={(e) => { if (e.key === 'Enter') { setSelectedStat("failedTarget") } }}
+                    onClick={() => setSelectedStat("failedTarget")}
                 >
-                    <Card style={cardStyle}>
+                    <Card style={selectedStat === "failedTarget" ? selectedCardStyle : cardStyle}>
                         <Statistic
                             loading={loading}
                             title={t("admin.reports.failed")}
@@ -156,10 +214,10 @@ export const AdminReports = () => {
                     role="button"
                     tabIndex={0}
                     style={{ cursor: "pointer" }}
-                    onKeyDown={(e) => { if (e.key === 'Enter') { setDatasetList(goodDatasets) } }}
-                    onClick={() => setDatasetList(goodDatasets)}
+                    onKeyDown={(e) => { if (e.key === 'Enter') { setSelectedStat("exceededTarget") } }}
+                    onClick={() => setSelectedStat("exceededTarget")}
                 >
-                    <Card style={cardStyle}>
+                    <Card style={selectedStat === "exceededTarget" ? selectedCardStyle : cardStyle}>
                         <Statistic
                             loading={loading}
                             title={t("admin.reports.exceeds")}
@@ -176,14 +234,13 @@ export const AdminReports = () => {
                     role="button"
                     tabIndex={0}
                     style={{ cursor: "pointer" }}
-                    onKeyDown={(e) => { if (e.key === 'Enter') { setDatasetList(overdueDatasetsListItems) } }}
-                    onClick={() => setDatasetList(overdueDatasetsListItems)}
-
+                    onKeyDown={(e) => { if (e.key === 'Enter') { setSelectedStat("overdue") } }}
+                    onClick={() => setSelectedStat("overdue")}
                 >
-                    <Card style={cardStyle}>
+                    <Card style={selectedStat === "overdue" ? selectedCardStyle : cardStyle}>
                         <Statistic
                             loading={loading}
-                            title={t("admin.reports.reqAttentionStatTitle")}
+                            title={t("admin.reports.overdueStatTitle")}
                             value={overdueDatasets.length}
                             precision={0}
                             valueStyle={{ color: "red" }}
@@ -196,13 +253,13 @@ export const AdminReports = () => {
                     role="button"
                     tabIndex={0}
                     style={{ cursor: "pointer" }}
-                    onKeyDown={(e) => { if (e.key === 'Enter') { setDatasetList(undefined) } }}
-                    onClick={() => setDatasetList(undefined)}
+                    onKeyDown={(e) => { if (e.key === 'Enter') { setSelectedStat("needsAttention") } }}
+                    onClick={() => setSelectedStat("needsAttention")}
                 >
-                    <Card style={cardStyle}>
+                    <Card style={selectedStat === "needsAttention" ? selectedCardStyle : cardStyle}>
                         <Statistic
                             loading={loading}
-                            title={t("admin.reports.reqImprovementStatTitle")}
+                            title={t("admin.reports.reqAttentionStatTitle")}
                             value={1 / 0}
                             precision={0}
                             valueStyle={{ color: "green" }}
@@ -244,31 +301,18 @@ export const AdminReports = () => {
                         size="small"
 
                     >
-                        {
-                            datasetList?.map((x) => {
-                                return <List.Item
-                                    key={x.key}
-                                    actions={[
-                                        <Space key={"email"} style={{ visibility: activeListItem === x.key ? "visible" : "hidden" }}>
-                                            <MailOutlined /><Button type="text">{t("admin.reports.emailTeam")}</Button>
-                                        </Space>,
-                                        <Space key={"alert"} style={{ visibility: activeListItem === x.key ? "visible" : "hidden" }}>
-                                            <AlertOutlined /><Button type="text">{t("admin.reports.alertTeam")}</Button>
-                                        </Space>,
-                                    ]}
-                                    onMouseEnter={() => setActiveListItem(x.key)}
-                                    onMouseLeave={() => setActiveListItem(undefined)}
-                                    onClick={() => setActiveListItem((curr) => curr ? undefined : x.key)}
-
-                                >
-                                    <Space>
-                                        {
-                                            x.render()
-                                        }
-                                    </Space>
-                                </List.Item>
-                            })
-                        }
+                        <Table
+                            columns={dataListBaseColumns}
+                            dataSource={datasetList}
+                            onRow={(record) => {
+                                return {
+                                    onMouseEnter: () => setActiveListItem(record.key),
+                                    onMouseLeave: () => setActiveListItem(undefined),
+                                    onClick: () => setActiveListItem((curr) => curr ? undefined : record.key),
+                                    backgroundColor: activeListItem ? "cyan" : "unset"
+                                }
+                            }}
+                        />
                     </List>
                 </Col>
             </Row>
