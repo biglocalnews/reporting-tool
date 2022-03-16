c7ba0dbbee5623f2d59044a44d93de6e35772713
Robert Cobain <robert.cobain@bbc.co.uk>
2022-03-16 17:50:25 +0000

    New importer WIP

--- api/importer.py
+++ api/importer.py
@@ -1,6 +1,13 @@
+import sys
 import csv
+from typing import List, Set
 from datetime import datetime
+from unicodedata import category
 from uuid import uuid4
+
+from attr import attributes
+from black import target_version_option_callback
+from tensorboard import program
 from database import (
     Category,
     CategoryValue,
@@ -9,228 +16,498 @@ from database import (
     Organization,
     PersonType,
     Record,
+    ReportingPeriod,
     Tag,
     Target,
+    Track,
+    User,
     connection,
     Team,
     Program,
 )
 from settings import settings
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from sqlalchemy import func
 
-with open("5050-data.csv", "r") as csv_file:
-    session = connection()
-    bbc_org = session.query(Organization).get("15d89a19-b78d-4ee8-b321-043f26bdd48a")
-    team = None
-    team_name = "Unassigned"
+import requests
+from requests_kerberos import HTTPKerberosAuth
+from requests_ntlm import HttpNtlmAuth
+from settings import settings
+
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning
+)
+
+session = connection()
+bbc_org = session.query(Organization).get("15d89a19-b78d-4ee8-b321-043f26bdd48a")
+category_gender = session.query(Category).get("51349e29-290e-4398-a401-5bf7d04af75e")
+
+
+def auth():
+    if settings.debug:
+        # only works over zscaler bizarrely
+        return HTTPKerberosAuth()
+    return HttpNtlmAuth("national\\ni-app-tig", settings.app_account_pw)
+
+
+def get_api_programmes():
+
+    data = []
 
     try:
-        team = session.query(Team).filter(Team.name == team_name).one()
+        r = requests.get(
+            f"https://laravel-api.mobileapps.bbc.co.uk/50-50/programmes",
+            verify=False,
+            auth=auth(),
+        )
+        parsed_response = r.json()
+        if "data" in parsed_response:
+            data = parsed_response["data"]
+    except Exception as ex:
+        print(ex)
+
+    return data
+
+
+def get_api_groups():
+
+    data = []
+
+    try:
+        r = requests.get(
+            f"https://laravel-api.mobileapps.bbc.co.uk/50-50/groups",
+            verify=False,
+            auth=auth(),
+        )
+        parsed_response = r.json()
+        if "data" in parsed_response:
+            data = parsed_response["data"]
+    except Exception as ex:
+        print(ex)
+
+    return data
+
+
+def get_api_records(prog_id):
+
+    data = []
+
+    try:
+        r = requests.get(
+            f"https://laravel-api.mobileapps.bbc.co.uk/50-50/{prog_id}/entries",
+            verify=False,
+            auth=auth(),
+        )
+        parsed_response = r.json()
+        if "data" in parsed_response:
+            data = parsed_response["data"]
+    except Exception as ex:
+        print(ex)
+
+    return data
+
+
+def get_api_user(email):
+
+    user = None
+
+    try:
+        r = requests.get(
+            f"https://laravel-api.mobileapps.bbc.co.uk/people/query?email={email}",
+            verify=False,
+            auth=auth(),
+        )
+        parsed_response = r.json()
+        if "data" in parsed_response:
+            users = parsed_response["data"]
+            if len(users) > 1:
+                print(
+                    f"More than one user with the same email {email} found, that's weird"
+                )
+                print(users)
+                exit(1)
+            if not len(users):
+                print(f"User {email} not found in api")
+            if len(users) == 1:
+                user = users[0]
+    except Exception as ex:
+        print(ex)
+
+    return user
+
+
+def get_tag(tag_name):
+    tag_name = tag_name.strip()
+    if not tag_name:
+        return None
+    db_tag = None
+
+    try:
+        db_tag = (
+            session.query(Tag).filter(func.lower(Tag.name) == tag_name.lower()).one()
+        )
     except MultipleResultsFound as e:
-        print(e)
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
+        exit(1)
+    except NoResultFound as e:
+        print(f"Tag {tag_name} not found in 5050 db")
+
+    if not db_tag:
+        db_tag = Tag(
+            id=uuid4(),
+            name=tag_name,
+            tag_type="unassigned",
+            description="",
+        )
+        session.add(db_tag)
+
+    return db_tag
+
+
+def get_team(team_name):
+    db_team = None
+    team_name = team_name.strip()
+    if not team_name:
+        return None
+    try:
+        db_team = (
+            session.query(Team).filter(func.lower(Team.name) == team_name.lower()).one()
+        )
+    except MultipleResultsFound as e:
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
         exit(1)
     except NoResultFound as e:
         print(e)
-        print("Creating new team")
-        team = Team(id=uuid4(), name=team_name, organization=bbc_org)
-        session.add(team)
 
-    programme = None
-    programme_name = "Unassigned"
+    if not db_team:
+        print(f"Team {team_name} not found, creating new team")
+        db_team = Team(id=uuid4(), name=team_name, organization=bbc_org)
+        session.add(db_team)
+
+    return db_team
+
+
+def get_user(email):
+    email = email.strip()
+    if not email or "@" not in email:
+        print(f"non email address {email}")
+        return None
+    email = email.lower()
+    db_user = None
+    try:
+        db_user = session.query(User).filter(func.lower(User.email) == email).one()
+    except MultipleResultsFound as e:
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
+        exit(1)
+    except NoResultFound as e:
+        print(f"User {email} not found in 5050 db")
+
+    if not db_user:
+        api_user = get_api_user(email)
+        if not api_user:
+            return None
+        if (
+            "accountName" not in api_user
+            or "givenName" not in api_user
+            or "surname" not in api_user
+        ):
+            print(f"{email} doesn't return all the normal metadata from the api")
+            return None
+        db_user = User(
+            id=uuid4(),
+            first_name=api_user["givenName"],
+            last_name=api_user["surname"],
+            username=api_user["accountName"].lower(),
+            email=email,
+            hashed_password=hash(uuid4()),
+        )
+        session.add(db_user)
+    return db_user
+
+
+def get_attribute(attribute_name, db_category):
+
+    attribute_name = attribute_name.strip()
+
+    if not attribute_name:
+        return None
+
+    db_attribute = None
 
     try:
-        programme = (
-            session.query(Program)
-            .filter(Program.name == programme_name, Program.team_id == team.id)
+        db_attribute = (
+            session.query(CategoryValue)
+            .filter(func.lower(CategoryValue.name) == attribute_name.lower())
             .one()
         )
     except MultipleResultsFound as e:
-        print(e)
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
         exit(1)
     except NoResultFound as e:
-        print(e)
-        print("Creating new programme")
-        programme = Program(id=uuid4(), name=programme_name, description="", team=team)
-        session.add(programme)
+        print(f"Creating new attribute {attribute_name}")
 
-    category_gender = session.query(Category).get(
-        "51349e29-290e-4398-a401-5bf7d04af75e"
-    )
+    if not db_attribute:
+        db_attribute = CategoryValue(
+            id=uuid4(), name=attribute_name, category=db_category
+        )
+        session.add(db_attribute)
+    return db_attribute
 
-    cat_value_cis_women = None
+
+def get_target(db_category, db_programme):
+    # assumes Gender because thats all we have in the old data
+    target = None
 
     try:
-        cat_value_cis_women = (
-            session.query(CategoryValue).filter(CategoryValue.name == "Women").one()
-        )
-    except MultipleResultsFound as e:
-        print(e)
-        exit(1)
-    except NoResultFound as e:
-        print("Creating new cat_value_cis_women")
-        cat_value_cis_women = CategoryValue(
-            id=uuid4(), name="women", category=category_gender
-        )
-        session.add(cat_value_cis_women)
-        print(cat_value_cis_women)
-
-    cat_value_cis_men = None
-
-    try:
-        cat_value_cis_men = (
-            session.query(CategoryValue).filter(CategoryValue.name == "Men").one()
-        )
-    except MultipleResultsFound as e:
-        print(e)
-        exit(1)
-    except NoResultFound as e:
-        print("Creating new cat_value_cis_men")
-        cat_value_cis_men = CategoryValue(
-            id=uuid4(), name="men", category=category_gender
-        )
-        session.add(cat_value_cis_men)
-        print(cat_value_cis_men)
-
-    target_cis_women = None
-
-    try:
-        target_cis_women = (
+        target = (
             session.query(Target)
             .filter(
-                Target.program_id == programme.id,
-                Target.category_value_id == cat_value_cis_women.id,
+                Target.program_id == db_programme.id, Target.category == db_category
             )
             .one()
         )
     except MultipleResultsFound as e:
-        print(e)
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
         exit(1)
     except NoResultFound as e:
-        print("Creating new target cis_women")
-        target_cis_women = Target(
+        print(f"Creating new target for {db_category.name}")
+
+    if not target:
+        target = Target(
             id=uuid4(),
-            program=programme,
-            category_value=cat_value_cis_women,
+            program=db_programme,
+            category=db_category,
             target_date=datetime.strptime("2022-12-31 00:00:00", "%Y-%m-%d %H:%M:%S"),
             target=float(0.50),
         )
-        session.add(target_cis_women)
+        session.add(target)
 
-    target_cis_men = None
+    target.tracks.append(
+        Track(
+            id=uuid4(),
+            category_value=get_attribute("Women", category_gender),
+            target_member=True,
+        )
+    )
+    target.tracks.append(
+        Track(
+            id=uuid4(),
+            category_value=get_attribute("Men", category_gender),
+            target_member=False,
+        )
+    )
+
+    return target
+
+
+def get_programme(programme_name):
+    programme_name = programme_name.strip()
+    if not programme_name:
+        return None
+    db_programme = None
 
     try:
-        target_cis_men = (
-            session.query(Target)
+        db_programme = (
+            session.query(Program)
+            .filter(func.lower(Program.name) == programme_name.lower())
+            .one()
+        )
+    except MultipleResultsFound as e:
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
+        exit(1)
+    except NoResultFound as e:
+        print(f"Program {programme_name} not found in 5050 db")
+
+    if not db_programme:
+        db_programme = Program(id=uuid4(), name=programme_name, description="")
+        session.add(db_programme)
+
+    return db_programme
+
+
+def get_person_type(person_type_name):
+    person_type_name = person_type_name.strip()
+    if not person_type_name:
+        return None
+    db_person_type = None
+
+    try:
+        db_person_type = (
+            session.query(PersonType)
+            .filter(func.lower(PersonType.person_type_name) == person_type_name.lower())
+            .one()
+        )
+    except MultipleResultsFound as e:
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
+        exit(1)
+    except NoResultFound as e:
+        print(f"PersonType {person_type_name} not found in 5050 db")
+
+    if not db_person_type:
+        db_person_type = PersonType(id=uuid4(), person_type_name=person_type_name)
+
+        session.add(db_person_type)
+
+    return db_person_type
+
+
+def get_dataset(programme, dataset_name):
+    dataset_name = dataset_name.strip()
+    if not dataset_name:
+        return None
+    db_dataset = None
+
+    try:
+        db_dataset = (
+            session.query(Dataset)
             .filter(
-                Target.program_id == programme.id,
-                Target.category_value_id == cat_value_cis_men.id,
+                Dataset.program == programme,
+                func.lower(Dataset.name) == dataset_name.lower(),
             )
             .one()
         )
     except MultipleResultsFound as e:
-        print(e)
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
         exit(1)
     except NoResultFound as e:
-        print("Creating new target cis_men")
-        target_cis_men = Target(
+        print(f"Dataset {dataset_name} not found in 5050 db")
+
+    if not db_dataset:
+        db_dataset = Dataset(
             id=uuid4(),
+            name=dataset_name,
+            description="",
+            person_types=[get_person_type("Everyone")],
             program=programme,
-            category_value=cat_value_cis_men,
-            target_date=datetime.strptime("2022-12-31 00:00:00", "%Y-%m-%d %H:%M:%S"),
-            target=float(0.0),
         )
-        session.add(target_cis_men)
+        session.add(db_dataset)
+
+    return db_dataset
+
+
+def get_record(dataset, publication_date):
+
+    if not publication_date:
+        return None
+
+    db_record = None
+
+    try:
+        db_record = (
+            session.query(Record)
+            .filter(
+                Record.dataset == dataset,
+                Record.publication_date == publication_date,
+            )
+            .one()
+        )
+    except MultipleResultsFound as e:
+        print(f"{sys._getframe(  ).f_code.co_name} {e}")
+        exit(1)
+    except NoResultFound as e:
+        print(f"Record for {publication_date} not found in 5050 db")
+
+    if not db_record:
+        db_record = Record(
+            id=uuid4(), publication_date=publication_date, dataset=dataset
+        )
+
+        session.add(db_record)
+
+    return db_record
+
+
+"""
+for group in get_api_groups():
+    if "groupName" not in group or not group["groupName"]:
+        continue
+    get_tag(group["groupName"])
+
+session.commit()
+"""
+
+for programme in get_api_programmes():
+
+    user_emails = (
+        programme["emails"].strip().split(";")
+        if "emails" in programme and len(programme["emails"]) > 8
+        else []
+    )
+
+    db_users: Set[User] = []
+
+    if len(user_emails) == 0:
+        print(f'programme {programme["name"]} has no contacts, that\'s weird')
+        continue
+
+    for email in set(user_emails):
+        db_user = get_user(email)
+        if db_user:
+            db_users.append(db_user)
+
+    db_team = get_team(programme["team"])
+
+    if db_team:
+        for db_user in db_users:
+            if not db_user.username.lower() in [
+                x.username.lower() for x in db_team.users
+            ]:
+                db_team.users.append(db_user)
+                print(
+                    f'{db_user.first_name} {db_user.last_name} added to Team {programme["team"]}'
+                )
+
+        session.add(db_team)
+
+    db_programme = get_programme(programme["name"])
+
+    if not db_programme.id in [x.id for x in db_team.programs]:
+        db_team.programs.append(db_programme)
+
+    db_dataset = get_dataset(db_programme, programme["name"])
+
+    if category_gender not in [x.category for x in db_programme.targets]:
+        db_programme.targets.append(get_target(category_gender, db_programme))
+
+    if (
+        "groupName" in programme
+        and programme["groupName"]
+        and programme["groupName"] not in [x.name for x in db_programme.tags]
+    ):
+        db_programme.tags.append(get_tag(programme["groupName"]))
+
+    if category_gender not in [x.category for x in db_programme.targets]:
+        db_programme.targets.append(get_target(category_gender, db_programme))
+
+    everyone_person_type = get_person_type("Everyone")
+
+    male_attribute = get_attribute("Men", category_gender)
+    female_attribute = get_attribute("Women", category_gender)
+
+    for record in get_api_records(programme["id"]):
+        db_record = get_record(
+            db_dataset,
+            datetime(day=1, month=record["month"], year=record["year"]),
+        )
+        if len(db_record.entries) == 0:
+            male_entry = Entry(
+                id=uuid4(),
+                category_value=male_attribute,
+                record=db_record,
+                count=record["maleRatio"],
+                person_type=everyone_person_type,
+            )
+            female_entry = Entry(
+                id=uuid4(),
+                category_value=female_attribute,
+                record_id=db_record.id,
+                count=record["femaleRatio"],
+                person_type=everyone_person_type,
+            )
+            session.add(male_entry)
+            session.add(female_entry)
+            db_record.entries.append(male_entry)
+            db_record.entries.append(female_entry)
+    db_programme.reporting_period_type = "monthly"
 
     session.commit()
-
-    csv_reader = csv.reader(
-        csv_file,
-        quotechar='"',
-        delimiter=",",
-        quoting=csv.QUOTE_ALL,
-        skipinitialspace=True,
-    )
-    next(csv_reader)
-    for line in csv_reader:
-        (
-            RemoteID,
-            ProgrammeId,
-            ProgrammeName,
-            GroupId,
-            GroupName,
-            Month,
-            Year,
-            MaleRatio,
-            FemaleRatio,
-        ) = line
-
-        print(ProgrammeName)
-
-        tag = None
-
-        try:
-            tag = session.query(Tag).filter(Tag.name == GroupName.strip()).one()
-        except MultipleResultsFound as e:
-            print(e)
-            exit(1)
-        except NoResultFound as e:
-            print(f"Creating new group name tag {GroupName}")
-            tag = Tag(
-                id=uuid4(), name=GroupName.strip(), description="", tag_type="imported"
-            )
-            session.add(tag)
-            session.commit()
-
-        dataset = None
-
-        try:
-            dataset = (
-                session.query(Dataset)
-                .filter(Dataset.name == ProgrammeName, Program.team_id == team.id)
-                .one()
-            )
-        except MultipleResultsFound as e:
-            print(e)
-            exit(1)
-        except NoResultFound as e:
-            print("Creating new dataset")
-            # BBC Contributor
-            ptype_id = "1c9b9573-726f-46c4-86a8-ed6412eb0c35"
-            ptype = session.query(PersonType).get(ptype_id)
-            dataset = Dataset(
-                id=uuid4(),
-                name=ProgrammeName,
-                description="",
-                tags=[tag],
-                person_types=[ptype],
-                program=programme,
-            )
-            session.add(dataset)
-
-        record = Record(
-            id=uuid4(),
-            dataset=dataset,
-            publication_date=datetime.strptime(
-                f"{Year}-{Month}-1 00:00:00", "%Y-%m-%d %H:%M:%S"
-            ),
-        )
-
-        session.add(record)
-
-        entry1 = Entry(
-            id=uuid4(),
-            count=int(FemaleRatio),
-            record=record,
-            category_value=cat_value_cis_women,
-        )
-        entry2 = Entry(
-            id=uuid4(),
-            count=int(MaleRatio),
-            record=record,
-            category_value=cat_value_cis_men,
-        )
-
-        session.add(entry1)
-        session.add(entry2)
-
-        session.commit()
